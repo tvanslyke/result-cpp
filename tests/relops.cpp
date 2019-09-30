7ba4ccfc1b19fb65a0c0243e594c010230eb2df9
Tim VanSlyke <vanslyke.t@husky.neu.edu>
2019-09-29 22:46:40 -0400

    Added partial relops test.

--- tests/relops.cpp
+++ tests/relops.cpp
@@ -1,6 +1,147 @@
 #include "catch.hpp"
 #include "tim/result/Result.hpp"
+#include <string>
 
-TEST_CASE("Relational operators", "[relops]") {
-    //TODO
+struct MyBool {
+	bool value;
+	constexpr explicit MyBool(bool v) : value(v) {}
+	constexpr operator bool() const noexcept { return value; }
+};
+
+struct ComparesToMyBool {
+	int value = 0;
+};
+
+inline constexpr MyBool operator==(const ComparesToMyBool& LHS, const ComparesToMyBool& RHS) noexcept {
+	return MyBool(LHS.value == RHS.value);
+}
+inline constexpr MyBool operator!=(const ComparesToMyBool& LHS, const ComparesToMyBool& RHS) noexcept {
+	return MyBool(LHS.value != RHS.value);
+}
+inline constexpr MyBool operator<(const ComparesToMyBool& LHS, const ComparesToMyBool& RHS) noexcept {
+	return MyBool(LHS.value < RHS.value);
+}
+inline constexpr MyBool operator<=(const ComparesToMyBool& LHS, const ComparesToMyBool& RHS) noexcept {
+	return MyBool(LHS.value <= RHS.value);
+}
+inline constexpr MyBool operator>(const ComparesToMyBool& LHS, const ComparesToMyBool& RHS) noexcept {
+	return MyBool(LHS.value > RHS.value);
+}
+inline constexpr MyBool operator>=(const ComparesToMyBool& LHS, const ComparesToMyBool& RHS) noexcept {
+	return MyBool(LHS.value >= RHS.value);
+}
+
+template <class L, class R>
+struct is_equality_comparable {
+	template <class T>
+	struct Tag { };
+	template <class T, class U, class = decltype(std::declval<T>() == std::declval<U>())>
+	static constexpr auto test(Tag<T>, Tag<U>, int) -> std::true_type;
+	template <class T, class U>
+	static constexpr auto test(Tag<T>, Tag<U>, ...) -> std::false_type;
+
+	static constexpr bool value = decltype(test(Tag<L>{}, Tag<R>{}, 0))::value;
+};
+
+template <class L, class R>
+inline constexpr bool is_equality_comparable_v = is_equality_comparable<L, R>::value;
+
+template <class L, class R>
+struct is_inequality_comparable {
+	template <class T>
+	struct Tag { };
+	template <class T, class U, class = decltype(std::declval<T>() != std::declval<U>())>
+	static constexpr auto test(Tag<T>, Tag<U>, int) -> std::true_type;
+	template <class T, class U>
+	static constexpr auto test(Tag<T>, Tag<U>, ...) -> std::false_type;
+
+	static constexpr bool value = decltype(test(Tag<L>{}, Tag<R>{}, 0))::value;
+};
+
+template <class L, class R>
+inline constexpr bool is_inequality_comparable_v = is_inequality_comparable<L, R>::value;
+
+template <class T1, class T2>
+void test_equality_basic() {
+	using tim::in_place;
+	using tim::in_place_error;
+	{
+		using V = tim::Result<T1, T2>;
+		constexpr V v1(in_place, T1{42});
+		constexpr V v2(in_place, T1{42});
+		static_assert(v1 == v2, "");
+		static_assert(v2 == v1, "");
+		static_assert(!(v1 != v2), "");
+		static_assert(!(v2 != v1), "");
+	}
+	{
+		using V = tim::Result<T1, T2>;
+		constexpr V v1(in_place, T1{42});
+		constexpr V v2(in_place, T1{43});
+		static_assert(!(v1 == v2), "");
+		static_assert(!(v2 == v1), "");
+		static_assert(v1 != v2, "");
+		static_assert(v2 != v1, "");
+	}
+	{
+		using V = tim::Result<T1, T2>;
+		constexpr V v1(in_place, T1{42});
+		constexpr V v2(in_place_error, T2{42});
+		static_assert(!(v1 == v2), "");
+		static_assert(!(v2 == v1), "");
+		static_assert(v1 != v2, "");
+		static_assert(v2 != v1, "");
+	}
+	{
+		using V = tim::Result<T1, T2>;
+		constexpr V v1(in_place_error, T2{42});
+		constexpr V v2(in_place_error, T2{42});
+		static_assert(v1 == v2, "");
+		static_assert(v2 == v1, "");
+		static_assert(!(v1 != v2), "");
+		static_assert(!(v2 != v1), "");
+	}
+}
+
+TEST_CASE("Equality/Inequality", "[relops]") {
+	test_equality_basic<int, long>();
+	test_equality_basic<ComparesToMyBool, int>();
+	test_equality_basic<int, ComparesToMyBool>();
+	test_equality_basic<ComparesToMyBool, ComparesToMyBool>();
+	struct T{};
+	struct U{};
+	using tim::Result;
+	using tim::Error;
+
+	REQUIRE(!is_equality_comparable_v<Result<T, int>, Result<U, int>>);
+	REQUIRE(!is_equality_comparable_v<Result<T, int>, Result<T, int>>);
+	REQUIRE(is_equality_comparable_v<Result<std::string, int>, Result<const char*, int>>);
+	REQUIRE(!is_inequality_comparable_v<Result<T, int>, Result<U, int>>);
+	REQUIRE(!is_inequality_comparable_v<Result<T, int>, Result<T, int>>);
+	REQUIRE(is_inequality_comparable_v<Result<std::string, int>, Result<const char*, int>>);
+
+	REQUIRE(tim::traits::detail::is_contextually_convertible_to_bool_v<decltype(std::declval<std::string>() == std::declval<const char*>())>);
+	REQUIRE(is_equality_comparable_v<Result<void, const char*>, Result<void, std::string>>);
+	REQUIRE(is_inequality_comparable_v<Result<void, const char*>, Result<void, std::string>>);
+	REQUIRE(!is_equality_comparable_v<Result<void, T>, Result<void, std::string>>);
+	REQUIRE(!is_inequality_comparable_v<Result<void, T>, Result<void, std::string>>);
+
+	REQUIRE(is_equality_comparable_v<Result<void, int>, Error<unsigned>>);
+	REQUIRE(is_inequality_comparable_v<Result<void, int>, Error<unsigned>>);
+	
+	REQUIRE(is_equality_comparable_v<Result<int, int>, Error<unsigned>>);
+	REQUIRE(is_inequality_comparable_v<Result<int, int>, Error<unsigned>>);
+	
+	REQUIRE(!is_equality_comparable_v<Result<int, T>, Error<unsigned>>);
+	REQUIRE(!is_inequality_comparable_v<Result<int, T>, Error<unsigned>>);
+	
+	REQUIRE(tim::Result<void, int>(Error(0)) == Error(0));
+	REQUIRE(!(tim::Result<void, int>(Error(0)) != Error(0)));
+
+	REQUIRE(tim::Result<std::string, int>("test") == Error(0));
+	REQUIRE(tim::Result<const char*, int>("test") != Error(0));
+
+	REQUIRE(tim::Result<const char*, int>(tim::in_place_error, 0) == Error(0));
+	REQUIRE(!(tim::Result<const char*, int>(tim::in_place_error, 0) != Error(0)));
+
 }
